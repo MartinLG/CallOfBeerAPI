9d9837bb14efc1f55c5694acb3d938da09eb66f0
Martin Le Guillou <martin.leguillou@y-nov.com>
2014-10-15 16:05:26 +0200

    add jmsSerializer and fosRest

---
+++ src/CallOfBeer/ApiBundle/CallOfBeerApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CallOfBeer\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CallOfBeerApiBundle extends Bundle
+{
+}
---
+++ src/CallOfBeer/ApiBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CallOfBeer\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CallOfBeerApiBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
