504f792052bb9d9251c2e0830b2bbb92efc12697
Martin Le Guillou <martin.leguillou@y-nov.com>
2014-12-03 09:55:00 +0100

    add cors for api

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new CallOfBeer\ApiBundle\CallOfBeerApiBundle(),
             new FOS\ElasticaBundle\FOSElasticaBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
